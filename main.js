d768272fca5af3f83a1a2e619571d2d13094b98f
Manojna <manojna.gopher@gmail.com>
2023-06-24 18:07:58 +0530

    feat: init

---
+++ main.js
@@ -0,0 +1,3 @@
+import { main } from "other.js";
+main();
+
---
+++ other.js
@@ -0,0 +1,11 @@
+function main() {
+	console.log("hello world");
+}
+
+function add(x, y) {
+	return x + y
+}
+
+export { main, add }
+
+
